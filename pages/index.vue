5732c913db509e8c7ca2b4f503ce8180b91415b2
LiuYan <361112237@qq.com>
2021-08-06 13:08:27 +0800

    fix(brand): update title if access by proxy

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ import { SEEN_WHATS_NEW } from '@/store/prefs';
 import { getVersionInfo } from '@/utils/version';
 
 export default {
-  middleware({ redirect, store } ) {
+  middleware({ redirect, store, route } ) {
     const seenWhatsNew = store.getters['prefs/get'](SEEN_WHATS_NEW);
     const versionInfo = getVersionInfo(store);
 
