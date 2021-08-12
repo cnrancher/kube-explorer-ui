78c1147438538850e11ac49f65f17c8d4325d2e9
LiuYan <361112237@qq.com>
2021-08-12 11:01:08 +0800

    fix(pages/index): redirect to cluster page

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,24 @@
 <script>
-import { SEEN_WHATS_NEW } from '@/store/prefs';
-import { getVersionInfo } from '@/utils/version';
+// import { SEEN_WHATS_NEW } from '@/store/prefs';
+// import { getVersionInfo } from '@/utils/version';
 
 export default {
-  middleware({ redirect, store, route } ) {
-    const seenWhatsNew = store.getters['prefs/get'](SEEN_WHATS_NEW);
-    const versionInfo = getVersionInfo(store);
+  // middleware({ redirect, store, route } ) {
+  //   const seenWhatsNew = store.getters['prefs/get'](SEEN_WHATS_NEW);
+  //   const versionInfo = getVersionInfo(store);
 
-    // If this is a new version, then take the user to the home page to view the release notes
-    if (versionInfo.fullVersion !== seenWhatsNew) {
-      return redirect({ name: 'home' });
-    }
+  //   // If this is a new version, then take the user to the home page to view the release notes
+  //   if (versionInfo.fullVersion !== seenWhatsNew) {
+  //     return redirect({ name: 'home' });
+  //   }
 
-    // Take the user to the configured login route
-    const afterLoginRouteObject = store.getters['prefs/afterLoginRoute'];
+  //   // Take the user to the configured login route
+  //   const afterLoginRouteObject = store.getters['prefs/afterLoginRoute'];
 
-    return redirect(afterLoginRouteObject);
+  //   return redirect(afterLoginRouteObject);
+  // }
+  middleware({ redirect } ) {
+    return redirect({ name: 'clusters' });
   }
 };
 </script>
