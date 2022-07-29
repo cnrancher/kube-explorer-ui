2e7ec60f868ea9b49e6d16ce77707dadad30764b
LiuYan <361112237@qq.com>
2022-07-29 13:55:33 +0800

    fix(nav): hide manager multi cluster feature link

--- shell/components/nav/TopLevelMenu.vue
+++ shell/components/nav/TopLevelMenu.vue
@@ -254,7 +254,7 @@ export default {
           </div>
         </div>
         <div class="body">
-          <div @click="hide()">
+          <!-- <div @click="hide()">
             <nuxt-link
               class="option cluster selector home"
               :to="{ name: 'home' }"
@@ -264,7 +264,7 @@ export default {
                 {{ t('nav.home') }}
               </div>
             </nuxt-link>
-          </div>
+          </div> -->
           <template v-if="clusters && !!clusters.length">
             <div class="category">
               {{ t('nav.categories.explore') }}
@@ -300,7 +300,7 @@ export default {
             </div>
           </template>
 
-          <template v-if="multiClusterApps.length">
+          <!-- <template v-if="multiClusterApps.length">
             <div class="category">
               {{ t('nav.categories.multiCluster') }}
             </div>
@@ -332,7 +332,7 @@ export default {
                 <div>{{ a.label }}</div>
               </nuxt-link>
             </div>
-          </template>
+          </template> -->
           <div class="pad"></div>
         </div>
         <div class="footer">
@@ -341,14 +341,14 @@ export default {
               {{ t('nav.support', {hasSupport}) }}
             </nuxt-link>
           </div>
-          <div @click="hide()">
+          <!-- <div @click="hide()">
             <nuxt-link
               v-tooltip="{ content: fullVersion, classes: 'footer-tooltip' }"
               :to="{ name: 'about' }"
               class="version"
               v-html="displayVersion"
             />
-          </div>
+          </div> -->
           <LocaleSelector></LocaleSelector>
         </div>
       </div>
