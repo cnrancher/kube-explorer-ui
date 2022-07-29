c1eb76ff3fe3e0b529fa0efb94e3a489ed19c4ba
LiuYan <361112237@qq.com>
2022-07-29 14:32:46 +0800

    fix(explorer): hide some links

--- shell/pages/c/_cluster/explorer/index.vue
+++ shell/pages/c/_cluster/explorer/index.vue
@@ -34,7 +34,7 @@ import Tab from '@shell/components/Tabbed/Tab';
 import { allDashboardsExist } from '@shell/utils/grafana';
 import EtcdInfoBanner from '@shell/components/EtcdInfoBanner';
 import metricPoller from '@shell/mixins/metric-poller';
-import EmberPage from '@shell/components/EmberPage';
+// import EmberPage from '@shell/components/EmberPage';
 import ResourceSummary, { resourceCounts } from '@shell/components/ResourceSummary';
 import HardwareResourceGauge from '@shell/components/HardwareResourceGauge';
 import { isEmpty } from '@shell/utils/object';
@@ -72,7 +72,7 @@ export default {
     Tabbed,
     AlertTable,
     Banner,
-    EmberPage,
+    // EmberPage,
     ConfigBadge,
     EventsTable,
     SimpleBox,
@@ -487,7 +487,7 @@ export default {
         <i class="icon icon-warning" />
       </p>
       <div :style="{'flex':1}" />
-      <div v-if="!monitoringStatus.v2 && !monitoringStatus.v1">
+      <!-- <div v-if="!monitoringStatus.v2 && !monitoringStatus.v1">
         <n-link
           :to="{name: 'c-cluster-explorer-tools'}"
           class="monitoring-install"
@@ -495,7 +495,7 @@ export default {
           <i class="icon icon-gear" />
           <span>{{ t('glance.installMonitoring') }}</span>
         </n-link>
-      </div>
+      </div> -->
       <div v-if="monitoringStatus.v1">
         <span>{{ t('glance.v1MonitoringInstalled') }}</span>
       </div>
@@ -585,7 +585,7 @@ export default {
       </div>
     </div>
 
-    <div
+    <!-- <div
       v-if="hasV1Monitoring"
       id="ember-anchor"
       class="mt-20"
@@ -594,7 +594,7 @@ export default {
         inline="ember-anchor"
         :src="v1MonitoringURL"
       />
-    </div>
+    </div> -->
 
     <div class="mt-30">
       <Tabbed @changed="tabChange">
