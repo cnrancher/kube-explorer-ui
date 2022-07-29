5955b53058cae71ef8dec51fc8fa238f471ebc7e
LiuYan <361112237@qq.com>
2022-07-29 12:05:08 +0800

    fix(api/router): fix api and router path error access after proxy

--- shell/nuxt.config.js
+++ shell/nuxt.config.js
@@ -230,7 +230,7 @@ export default function(dir, _appConfig) {
   }
 
   let routerBasePath = '/';
-  let resourceBase = '';
+  let resourceBase = './';
   let outputDir = 'dist';
 
   if ( typeof process.env.ROUTER_BASE !== 'undefined' ) {
