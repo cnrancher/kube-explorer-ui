2cc2a2abcf5eee6925dd26ec1b9d3fa621147d82
LiuYan <361112237@qq.com>
2022-07-29 14:37:56 +0800

    fix(fail-whale): change home link to base path

--- shell/pages/fail-whale.vue
+++ shell/pages/fail-whale.vue
@@ -27,11 +27,13 @@ export default {
     ...mapGetters(['isSingleProduct']),
 
     home() {
-      if (this.isSingleProduct?.afterLoginRoute) {
-        return this.$router.resolve(this.isSingleProduct.afterLoginRoute).href;
-      }
+      // if (this.isSingleProduct?.afterLoginRoute) {
+      //   return this.$router.resolve(this.isSingleProduct.afterLoginRoute).href;
+      // }
 
-      return this.$router.resolve({ name: 'home' }).href;
+      // return this.$router.resolve({ name: 'home' }).href;
+
+      return process.client ? document.querySelector('head > base').href : '/';
     },
 
     displayError() {
