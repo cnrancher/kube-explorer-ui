a7891e60b99b63c3ee2cf789e9a226bd8e4cb451
LiuYan <361112237@qq.com>
2022-08-25 16:33:02 +0800

    chore: support dev server

--- shell/plugins/axios.js
+++ shell/plugins/axios.js
@@ -16,7 +16,7 @@ export default function({
       config.headers['x-api-csrf'] = csrf;
     }
 
-    if ( config.url.startsWith('/') ) {
+    if ( !process.env.dev && config.url.startsWith('/') ) {
       config.baseURL = `${ getBasePath() }`;
     }
 
