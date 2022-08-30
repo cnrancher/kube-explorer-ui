0cb764194f936a0e996814d8aaa3d8e39fac46db
LiuYan <361112237@qq.com>
2022-08-30 15:32:11 +0800

    fix(product/explorer): disable cluster members nav

--- shell/config/product/explorer.js
+++ shell/config/product/explorer.js
@@ -58,7 +58,7 @@ export function init(store) {
     'projects-namespaces',
     'namespaces',
     NODE,
-    VIRTUAL_TYPES.CLUSTER_MEMBERS,
+    // VIRTUAL_TYPES.CLUSTER_MEMBERS, for kube-explorer ui
     EVENT,
   ], 'cluster');
   basicType([
