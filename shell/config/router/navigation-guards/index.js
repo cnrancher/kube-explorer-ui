b306f9202e9ea6cdae42d20ebbed11cbefb922b0
ly5156 <361112237@qq.com>
2024-08-20 14:54:21 +0800

    fix: support login and redirect cluster explorer page

--- shell/config/router/navigation-guards/index.js
+++ shell/config/router/navigation-guards/index.js
@@ -1,4 +1,4 @@
-// import { install as installLoadInitialSettings } from '@shell/config/router/navigation-guards/load-initial-settings';
+import { install as installLoadInitialSettings } from '@shell/config/router/navigation-guards/load-initial-settings';
 // import { install as installAttemptFirstLogin } from '@shell/config/router/navigation-guards/attempt-first-login';
 import { install as installAuthentication } from '@shell/config/router/navigation-guards/authentication';
 import { install as installRuntimeExtensionRoute } from '@shell/config/router/navigation-guards/runtime-extension-route';
@@ -11,7 +11,7 @@ export function installNavigationGuards(router, context) {
   // NOTE: the order of the installation matters.
   // Be intentional when adding, removing or modifying the guards that are installed.
 
-  const navigationGuardInstallers = [installAuthentication, installRuntimeExtensionRoute, installI18N];
+  const navigationGuardInstallers = [installLoadInitialSettings, installAuthentication, installRuntimeExtensionRoute, installI18N];
 
   navigationGuardInstallers.forEach((installer) => installer(router, context));
 }
