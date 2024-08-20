3639aed02b8f1f9d65a9e36f420dc59d5facddd6
ly5156 <361112237@qq.com>
2024-08-20 14:26:35 +0800

    fix(router): fix base url error

--- shell/config/router/index.js
+++ shell/config/router/index.js
@@ -5,10 +5,15 @@ import { installNavigationGuards } from '@shell/config/router/navigation-guards'
 
 Vue.use(Router);
 
+function getBasePath() {
+  return decodeURI(document.querySelector('head > base').href.replace(window.location.origin, ''));
+}
+
 export const routerOptions = {
   mode:     'history',
   // Note: router base comes from the ROUTER_BASE env var
-  base:     process.env.routerBase || '/',
+  // base:     process.env.routerBase || '/',
+  base:     getBasePath(),
   routes:   Routes,
   fallback: false
 };
